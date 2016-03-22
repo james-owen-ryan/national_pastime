709ba309594cf908f834f92c4e5c16a74a59a523
James Ryan <joryan@ucsc.edu>
2016-03-22 10:51:53 -0700

    Added class schedule.py

---
+++ baseball/schedule.py
@@ -0,0 +1,6 @@
+class Schedule(object):
+    """A schedule for a baseball league's season."""
+
+    def __init__(self, league):
+        """Initialize a Schedule object."""
+        self.league = league
