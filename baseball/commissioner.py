47979b6b1b4935d4989eac6e68b19654a188d291
James Ryan <joryan@ucsc.edu>
2016-03-22 10:51:41 -0700

    Added class commissioner.py

---
+++ baseball/commissioner.py
@@ -0,0 +1,6 @@
+class Commissioner(object):
+    """The baseball-commissioner layer of a person's being."""
+
+    def __init__(self, person):
+        """Initialize a Fan object."""
+        self.person = person  # The person in whom this commissioner layer embeds
