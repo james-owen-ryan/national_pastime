280e10ff02191d1cde4d2aac839cbd007d6bd50c
James Ryan <joryan@ucsc.edu>
2016-03-22 11:31:05 -0700

    Added class Scout

---
+++ baseball/scout.py
@@ -0,0 +1,6 @@
+class Scout(object):
+    """The baseball-scout layer of a person's being."""
+
+    def __init__(self, person):
+        """Initialize a Scout object."""
+        self.person = person  # The person in whom this fan layer embeds
