dbb8e5dc4ce60938ed6180b4de0acb0a0c1d74fe
James Ryan <joryan@ucsc.edu>
2016-03-25 10:54:06 -0700

    Added 'time_of_day' attribute to Event objects

--- people/event.py
+++ people/event.py
@@ -22,11 +22,13 @@ class Event(object):
         self.year = cosmos.year
         if self.year < cosmos.config.year_worldgen_begins:  # This event is being retconned; generate a random day
             self.month, self.day, self.ordinal_date = cosmos.get_random_day_of_year(year=self.year)
+            self.time_of_day = random.choice(['day', 'night'])
             self.date = cosmos.get_date(ordinal_date=self.ordinal_date)
         else:
             self.month = cosmos.month
             self.day = cosmos.day
             self.ordinal_date = cosmos.ordinal_date
+            self.time_of_day = cosmos.time_of_day
             self.date = cosmos.date
         # Also request and attribute an event number, so that we can later
         # determine the precise ordering of events that happen on the same timestep
